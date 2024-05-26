9300014c9bab426e1ec8e6f3b1687f68a49d8ac8
dongcheon1020 <dongcheon1020@gmail.com>
2024-05-26 21:54:15 +0900

    Feat: login

---
+++ model/User.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+
+const userSchema = Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.methods.toJSON = function () {
+  const obj = this._doc;
+  delete obj.password;
+  return obj;
+};
+
+userSchema.methods.generateToken = function () {
+  const token = jwt.sign({ _id: this._id }, JWT_SECRET_KEY, {
+    expiresIn: "1d",
+  });
+  return token;
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const taskApi = require("./task.app");
+const userAPi = require("./user.api");
 
 router.use("/tasks", taskApi);
+router.use("/user", userAPi);
 
 module.exports = router;
